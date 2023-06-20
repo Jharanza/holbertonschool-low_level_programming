31c9bf2d1972aaa138186a5d05d9737d33766516
jharanza <jharanza1986@gmail.com>
2023-06-20 14:03:16 -0700

    Addign blank line

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -10,8 +10,10 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
 	printf("%d is positive\n", n);
@@ -25,4 +27,5 @@ int main(void)
 	printf("%d is negative\n", n);
 	}
 	return (0);
+
 }
