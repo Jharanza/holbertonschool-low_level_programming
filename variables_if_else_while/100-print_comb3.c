db58869f20aeb512a1f041b147b5694ba645f1f5
jharanza <jharanza1986@gmail.com>
2023-06-21 13:22:55 -0700

    Printing numbers in pairs

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Print two pairs of different numbers
+ * Return: Always (Succes)
+ *
+ */
+
+int main (void)
+{
+	int i;
+	int j;
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (j != i && j > i)
+			{
+				putchar(i + 48);
+				putchar(j + 48);
+				if (i == 8 && j == 9)
+				{
+					break;
+				}
+				putchar(32);
+				putchar(44);
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
