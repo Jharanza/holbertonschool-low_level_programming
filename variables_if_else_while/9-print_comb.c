6096a90bfb130f8fb4dab71c0f2e19898c7df242
jharanza <jharanza1986@gmail.com>
2023-06-21 12:33:40 -0700

    Printing numbers, commas and spaces

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ * main - Prints single digits numbers with commas and space
+ * Return: Always (Succes)
+ *
+ */
+
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + 48);
+		if (n == 9)
+		{
+			break;
+		}
+		putchar(44);
+		putchar(32);
+	}
+
+	return (0);
+
+}
