863630601b9834b6a8e989554ae917ed725663b4
jharanza <jharanza1986@gmail.com>
2023-06-21 07:31:08 -0700

    Printing numbers form 0 to 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ * main - Print numbers from 0 to 9 
+ * Return: Always (Succes)
+ *
+ */
+
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+
+	return (0);
+
+}
