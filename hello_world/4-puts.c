e8a2e419efab8e032e8a1d82eb08ab59a55969d8
jharanza <jharanza1986@gmail.com>
2023-06-23 13:46:37 -0700

    Script-Printing a statement and a new line

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/** main - Prints a statement followed by a new line.
+ *
+ * Return : Always
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return(0);
+}
