0843bb844f429b8c2c6a27286a8568ed3abff838
jharanza <jharanza1986@gmail.com>
2023-06-24 10:25:34 -0700

    Checking if a variable is digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isupper - Check if a vairable is a digit
+ *
+ * @c: variable to check
+ *
+ * Return: 1 if c is a digit, 0 in any another way
+ *
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
