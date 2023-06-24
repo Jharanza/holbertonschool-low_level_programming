de33cc2e1d0ecb11700abfecc60567c49a08269b
jharanza <jharanza1986@gmail.com>
2023-06-24 12:20:29 -0700

    Print 10 times form 0 to 14

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - Pirnt 10 times from 0 - 14
+ *
+ * Return: void
+ *
+ */
+
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y >10)
+			{
+				_putchar((y / 10) + '0');
+			}
+			_putchar((y % 10) + '0');
+		}
+		_putchar(10);
+	}
+}
