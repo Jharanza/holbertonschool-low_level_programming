f2e6f170486c96c71269e1c5657cb21cbb4ae6b6
jharanza <jharanza1986@gmail.com>
2023-06-24 22:41:48 -0700

    Sum two numbers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Sum two numbers
+ *
+ * @x: Variable in operation
+ *
+ * @y: Varibale in operation
+ *
+ * Return: Sum of x and y
+ */
+
+int add(int x, int y)
+{
+	int result = x + y;
+
+	return (result);
+}
