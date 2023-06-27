9ec6897444060f115d871605a91a8b31af8f0e6c
jharanza <jharanza1986@gmail.com>
2023-06-27 10:17:54 -0700

    Updating main.h file

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,8 @@
+#ifndef _main_h_
+#define _main_h_
+
+int _putchar(char c);
+
 void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
@@ -19,3 +24,5 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
 int _atoi(char *s);
+
+#endif
