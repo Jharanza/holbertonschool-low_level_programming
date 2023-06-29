f435f64d0b0ef851bd08576a48a8705ab8a9a7f5
jharanza <jharanza1986@gmail.com>
2023-06-29 09:14:21 -0700

    Printing a string left and right

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - Print a string rigth and  reverse
+ *
+ * @s: String to be reversed
+ *
+ * Return: void
+ *
+ */
+
+void rev_string(char *s)
+{
+
+	int c = 0;
+	int d = 0;
+	char change;
+
+	while (s[d++])
+	{
+		c++;
+	}
+
+	for (d = c - 1; d >= c / 2; d--)
+	{
+		change = s[d];
+		s[d] = s[c - d - 1];
+		s[c - d -1] = change;
+	}
+}
