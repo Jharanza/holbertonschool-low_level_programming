916a396987af5b9eab2ff7369b9126c26c0b5180
jharanza <jharanza1986@gmail.com>
2023-07-03 13:05:31 -0700

    Defining a new type: struct

---
+++ structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef _dog_h_
+
+#define _dog_h_
+/**
+ * struct dog - A new structure
+ *
+ * @name: Pointer to a char variable
+ *
+ * @owner: 2do pointer to another char variable
+ *
+ * @age: Variable type float
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
