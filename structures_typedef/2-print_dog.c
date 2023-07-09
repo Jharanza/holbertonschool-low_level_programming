f0bb42a65cce881e56928679e3cc552e8ccaf745
jharanza <jharanza1986@gmail.com>
2023-07-09 07:59:09 -0700

    Changing the code

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,31 +14,13 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-		if (d->name)
-		{
-			printf("Name: %s\n", d->name);
-		}
-		else
-		{
-			printf("Name: %s\n", "(nil)");
-		}
-
-		if (d->age)
-		{
-			printf("Age: %f\n", d->age);
-		}
-		else
-		{
-			printf("age: %.u:\n", 0);
-		}
-
-		if (d->owner)
-		{
-			printf("Owner: %s\n", d->owner);
-		}
-		else
-		{
-			printf("Owner: %s\n", "(nil)");
-		}
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	}
+	else
+	{
+		if (d->name == NULL) d->name = "(nil)";
+		if (d->owner == NULL) d->owner = "(nil)";
+		
+		return ;	
 	}
 }
