1e7f329118e7c3412d275aa6545fef4f4bb8ed3a
jharanza <jharanza1986@gmail.com>
2023-07-09 09:51:36 -0700

    function that prints a name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name
+ *
+ * @name: pointer to a character
+ *
+ * @f: Name of the function ponter
+ *
+ * @(+f)(char *): function pointer
+ *
+ * Return: void
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ function_pointers/README.md
@@ -0,0 +1,4 @@
+This directory talk aobout:
+	What are function pointers and how to use them
+	What does a function pointer exactly hold
+	Where does a function pointer point to in the virtual memory
